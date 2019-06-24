9e9154edb3fad1be2f5e4cc7e51b1f2fda157aa7
joaoh <joao_camargo10@hotmail.com>
2019-06-24 19:54:27 -0300

    Finishing top layout

--- lib/custom_shape_clipper.dart
+++ lib/custom_shape_clipper.dart
@@ -11,8 +11,8 @@ class CustomShapeClipper extends CustomClipper<Path> {
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
 
-    var secondEndPoint = Offset(size.width, size.height - 80.0);
-    var secondControlPoint = Offset(size.width * 0.75, size.height - 10.0);
+    var secondEndPoint = Offset(size.width, size.height - 60.0);
+    var secondControlPoint = Offset(size.width * 0.75, size.height - 3.0);
 
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,8 @@ class HomeScreenTopPart extends StatefulWidget {
 }
 
 class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
-
   var selectedLocationIndex = 0;
+  var isFlightSelected = true;
 
   @override
   Widget build(BuildContext context) {
@@ -56,13 +56,13 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
         ClipPath(
           clipper: CustomShapeClipper(),
           child: Container(
-            height: 400.0,
+            height: 340.0,
             decoration: BoxDecoration(
                 gradient: LinearGradient(colors: [firstColor, secondColor])),
             child: Column(
               children: <Widget>[
                 SizedBox(
-                  height: 50.0,
+                  height: 40.0,
                 ),
                 Padding(
                   padding: const EdgeInsets.all(16.0),
@@ -76,7 +76,7 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                         width: 16.0,
                       ),
                       PopupMenuButton(
-                        onSelected: (index){
+                        onSelected: (index) {
                           setState(() {
                             selectedLocationIndex = index;
                           });
@@ -109,15 +109,123 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                                 value: 1,
                               )
                             ],
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.settings,
+                        color: Colors.white,
                       )
                     ],
                   ),
                 ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Where would\nyou want to go?",
+                  style: TextStyle(fontSize: 22.0, color: Colors.white, fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32),
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    child: TextField(
+                      controller: TextEditingController(text: locations[1]),
+                      style: dropDownMenuItemStyle,
+                      cursorColor: appTheme.primaryColor,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 32.0, vertical: 14.0),
+                          suffixIcon: Material(
+                            elevation: 2.0,
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                            ),
+                          )),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          isFlightSelected = true;
+                        });
+                      },
+                      child: ChoiceChip(
+                          Icons.flight_takeoff, "Flights", isFlightSelected),
+                    ),
+
+                    InkWell(
+                        onTap: () {
+                          setState(() {
+                            isFlightSelected = false;
+                          });
+                        },
+                        child: ChoiceChip(
+                            Icons.hotel, "Hotels", !isFlightSelected))
+                  ],
+                )
               ],
             ),
           ),
         )
       ],
+    );
+  }
+}
+
+class ChoiceChip extends StatefulWidget {
+  final IconData icon;
+  final String text;
+  final bool isFlightSelected;
+
+  ChoiceChip(this.icon, this.text, this.isFlightSelected);
+
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<ChoiceChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
+      decoration: widget.isFlightSelected
+          ? BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)))
+          : null,
+      child: Row(
+        children: <Widget>[
+          Icon(
+            widget.icon,
+            size: 18.0,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 4.0,
+          ),
+          Text(
+            widget.text,
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          )
+        ],
+      ),
     );
   }
 }
